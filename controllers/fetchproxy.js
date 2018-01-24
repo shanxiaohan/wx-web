b0bca9b8b7307fb2c8e4b075f8fe5f6f4a6c0aba
shanxiaohan <1263108183@qq.com>
2018-01-24 12:55:45 +0800

    test

--- controllers/fetchproxy.js
+++ controllers/fetchproxy.js
@@ -3,13 +3,19 @@ const fs = require('fs');
 module.exports = {
   'POST /fetchHistList': async (ctx, next) => {
     let url = ctx.request.body.url,
-        content = ctx.request.body.content;
+        content = ctx.request.body.content,
+	params = ctx.request.body.params;
     console.log(url, '****url****');
-    console.log(content, '****content****');
-    fs.readFile('test.txt',content, function(err){ 
+    console.log(params, '****content****');
+    // 将url, content格式化后存入数据库
+    fs.writeFile('test.txt',content, function(err){ 
       if(err){ 
         console.log(err); 
       }
     });
+    ctx.response.body = {code: 200, msg:'success'};
+  },
+
+  'POST /fetchMsgJson': async(ctx, next) => {
   }
 };
